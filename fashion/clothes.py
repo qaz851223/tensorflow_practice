a6ea686922900eb0f72deeb5b37b019bc7e75423
christy.hsieh <christy.hsieh@vizionfocus.com>
2023-11-27 14:11:17 +0800

    fashion_mnist

--- fashion/clothes.py
+++ fashion/clothes.py
@@ -8,31 +8,54 @@ import numpy as np
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels),(test_images, test_labels) = fashion_mnist.load_data()
 
-# print(train_labels[:5])
-# plt.imshow(train_images[1])
-# plt.show()
+# 數據及布包刮名稱類，之後繪製圖像時使用
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+print(train_labels[:5])
+print(train_images.shape)
+print(len(train_labels))
+print(train_labels)
+print(test_images.shape)
+print(len(test_labels))
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+# ---------預處理---------
+train_images = train_images / 255
+test_images = test_images / 255
+
+# 查看驗證數據的格式是否正確
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
 
 # ---------建構模型---------
-model = keras.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28, 28)))
-model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10))
 model.summary()
-# 100480 = (784+1)*128    +1代表輸入層跟中間層都+bias
-# 1290 = (128+1)*10    輸出層10個類別(0~9)
 
 # ---------訓練&評估模型---------
-train_images = train_images/255
-model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-model.fit(train_images,train_labels, epochs=5)
+model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=10)
+# ---------評估準確率---------
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
 
-test_images_scaled = test_images/255
-model.evaluate(test_images_scaled, test_labels)
-
-# print(model.predict([[test_images/255]])[0])
-# print(np.argmax(model.predict([[test_images/255]])[0]))
-# print(test_images[0])
-# plt.imshow(test_images[0])
-# plt.show()
+print(model.predict([[test_images]])[0])
+print(np.argmax(model.predict([[test_images]])[0]))
+print(test_images[0])
+plt.imshow(test_images[0])
+plt.show()
 
